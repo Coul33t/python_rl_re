d5adba1252e8fb4ca87be19cf5d7f031e5912d98
Coul33t <Coulis1990@gmail.com>
2018-05-03 11:25:21 +0200

    Changed the inherited class

--- item.py
+++ item.py
@@ -1,5 +1,5 @@
-from living_entity import *
+from generic_object import *
 
-class Item(LivingEntity):
+class Item(GenericObject):
     def __init__(self, x, y, ch='o', name='item'):
         super(Item, self).__init__(x, y, ch, name, color=(100,100,255))
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
-from living_entity import *
+from actor import *
 
-class Player(LivingEntity):
+class Player(Actor):
     def __init__(self, x, y, ch='@'):
         super(Player, self).__init__(x, y, ch, name='player')
