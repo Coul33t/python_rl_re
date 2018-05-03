3825ad6f423249d35bed0057ad515376e714f23b
Coul33t <Coulis1990@gmail.com>
2018-05-03 11:08:58 +0200

    Got rid of useless mutators

--- game_map.py
+++ game_map.py
@@ -23,44 +23,11 @@ MAX_ROOM_SIZE = 10
 
 class GameMap:
     def __init__(self, width, height):
-        self._width = width
-        self._height = height
-        self._map_array = [[Tile(MAP_TILES['wall'], color=light_gray) for y in range(height)] for x in range(width)]
-        self._rooms = []
-
-    def _get_width(self):
-        return self._width
-
-    def _set_width(self, width):
-        self._width = width
-
-    width = property(_get_width, _set_width)
-
-    def _get_height(self):
-        return self._height
-
-    def _set_height(self, height):
-        self._height = height
-
-    height = property(_get_height, _set_height)
-
-    def _get_map_array(self):
-        return self._map_array
-
-    def _set_map_array(self, map_array):
-        self._map_array = map_array
-
-    map_array = property(_get_map_array, _set_map_array)
-
-    def _get_rooms(self):
-        return self._rooms
-
-    def _set_rooms(self, rooms):
-        pass
-
-    rooms = property(_get_rooms, _set_rooms)
-
-
+        self.width = width
+        self.height = height
+        self.map_array = [[Tile(MAP_TILES['wall'], color=light_gray) for y in range(height)] for x in range(width)]
+        self.rooms = []
+        self.visible_tiles = []
 
 
     def is_visible_tile(self, x, y):
@@ -73,10 +40,10 @@ class GameMap:
         elif y >= MAP_HEIGHT or y < 0:
             return False
 
-        elif self._map_array[x][y].blocked:
+        elif self.map_array[x][y].blocked:
             return False
 
-        elif self._map_array[x][y].block_sight:
+        elif self.map_array[x][y].block_sight:
             return False
 
         else:
@@ -94,27 +61,27 @@ class GameMap:
 
 
     def clear_map(self):
-        self._map_array = [[Tile(MAP_TILES['wall'], color=light_gray) for y in range(self._height)] for x in range(self._width)]
+        self.map_array = [[Tile(MAP_TILES['wall'], color=light_gray) for y in range(self.height)] for x in range(self.width)]
 
 
     def create_room(self, room):
         for x in range(room.x1, room.x2):
             for y in range(room.y1, room.y2):
-                self._map_array[x][y].ch = MAP_TILES['floor']
-                self._map_array[x][y].blocked = False
-                self._map_array[x][y].block_sight = False
+                self.map_array[x][y].ch = MAP_TILES['floor']
+                self.map_array[x][y].blocked = False
+                self.map_array[x][y].block_sight = False
 
     def carve_h_tunnel(self, x1, x2, y):
         for x in range(min(x1, x2), max(x1, x2) + 1):
-            self._map_array[x][y].ch = MAP_TILES['floor']
-            self._map_array[x][y].blocked = False
-            self._map_array[x][y].block_sight = False
+            self.map_array[x][y].ch = MAP_TILES['floor']
+            self.map_array[x][y].blocked = False
+            self.map_array[x][y].block_sight = False
 
     def carve_v_tunnel(self, y1, y2, x):
         for y in range(min(y1, y2), max(y1, y2) + 1):
-            self._map_array[x][y].ch = MAP_TILES['floor']
-            self._map_array[x][y].blocked = False
-            self._map_array[x][y].block_sight = False
+            self.map_array[x][y].ch = MAP_TILES['floor']
+            self.map_array[x][y].blocked = False
+            self.map_array[x][y].block_sight = False
 
 
 
@@ -142,13 +109,13 @@ class GameMap:
 
                 w = rn.randint(MIN_ROOM_SIZE, MAX_ROOM_SIZE)
                 h = rn.randint(MIN_ROOM_SIZE, MAX_ROOM_SIZE)
-                x = rn.randint(1, self._width - w - 1)
-                y = rn.randint(1, self._height - h - 1)
+                x = rn.randint(1, self.width - w - 1)
+                y = rn.randint(1, self.height - h - 1)
 
                 new_room = Rect(x, y, w, h)
 
-                if self._rooms:
-                    for other_room in self._rooms:
+                if self.rooms:
+                    for other_room in self.rooms:
                         if new_room.intersect(other_room):
                             carved = False
                 else:
@@ -163,7 +130,7 @@ class GameMap:
 
             else:
                 closest_room = [-1, -1]
-                for i, other_room in enumerate(self._rooms):
+                for i, other_room in enumerate(self.rooms):
                     if closest_room == [-1, -1]:
                         closest_room = list(other_room.get_center())
                     else:
@@ -178,7 +145,7 @@ class GameMap:
                     self.carve_h_tunnel(x, closest_room[0], closest_room[1])
 
 
-            self._rooms.append(new_room)
+            self.rooms.append(new_room)
 
             num_rooms += 1
 
@@ -187,25 +154,20 @@ class GameMap:
 
 
 
-    def draw_map(self, fov_map, player_x, player_y, display_x, display_y, map_console):
+    def draw_map(self, fov_map, recompute, player_x, player_y, display_x, display_y, map_console):
 
-        visible_tiles = []
-
-        visible_tiles_iter = fov_map.compute_fov(player_x, player_y, radius=TORCH_RADIUS, light_walls=FOV_LIGHT_WALLS)
-
-
-        for tile in visible_tiles_iter:
-            visible_tiles.append(tile)
-
+        if recompute or not visible_tiles:
+            visible_tiles_iter = fov_map.compute_fov(player_x, player_y, radius=TORCH_RADIUS, light_walls=FOV_LIGHT_WALLS)
+            self.visible_tiles = list(visible_tiles_iter)
 
         for x in range(display_x):
             for y in range(display_y):
 
-                if (x, y) in visible_tiles:
-                    self._map_array[x][y].explored = True
-                    map_console.draw_char(x, y, self._map_array[x][y].ch, fg=VISIBLE_COLORS[self._map_array[x][y].ch], bg=self._map_array[x][y].bkg_color)
+                if (x, y) in self.visible_tiles:
+                    self.map_array[x][y].explored = True
+                    map_console.draw_char(x, y, self.map_array[x][y].ch, fg=VISIBLE_COLORS[self.map_array[x][y].ch], bg=self.map_array[x][y].bkg_color)
                 else:
-                    if self._map_array[x][y].explored:
-                        map_console.draw_char(x, y, self._map_array[x][y].ch, fg=NOT_VISIBLE_COLORS[self._map_array[x][y].ch], bg=self._map_array[x][y].bkg_color)
+                    if self.map_array[x][y].explored:
+                        map_console.draw_char(x, y, self.map_array[x][y].ch, fg=NOT_VISIBLE_COLORS[self.map_array[x][y].ch], bg=self.map_array[x][y].bkg_color)
 
-        return visible_tiles
+        return self.visible_tiles
--- generic_object.py
+++ generic_object.py
@@ -1,83 +1,14 @@
 class GenericObject:
     def __init__(self, x, y, ch='X', name='NO_NAME', color=(255,255,255), bkg_color=None, blocks=True, always_visible=False):
-        self._x = x
-        self._y = y
-        self._ch = ch
-        self._name = name
-        self._color = color
-        self._bkg_color = bkg_color
-        self._blocks = blocks
-        self._always_visible = always_visible
-
-    def _get_x(self):
-        return self._x
-
-    def _set_x(self, x):
-        self._x = x
-
-    x = property(_get_x, _set_x)
-
-    def _get_y(self):
-        return self._y
-
-    def _set_y(self, y):
-        self._y = y
-
-    y = property(_get_y, _set_y)
-
-    def _get_ch(self):
-        return self._ch
-
-    def _set_ch(self, ch):
-        self._ch = ch
-
-    ch = property(_get_ch, _set_ch)
-
-    def _get_color(self):
-        return self._color
-
-    def _set_color(self, color):
-        self._color = color
-
-    color = property(_get_color, _set_color)
-
-    def _get_bkg_color(self):
-        return self._bkg_color
-
-    def _set_bkg_color(self, bkg_color):
-        self._bkg_color = bkg_color
-
-    bkg_color = property(_get_bkg_color, _set_bkg_color)
-
-    def _get_name(self):
-        return self._name
-
-    def _set_name(self, name):
-        self._name = name
-
-    name = property(_get_name, _set_name)
-
-    def _get_blocks(self):
-        return self._blocks
-
-    def _set_blocks(self, blocks):
-        self._blocks = blocks
-
-    blocks = property(_get_blocks, _set_blocks)
-
-    def _get_always_visible(self):
-        return self._always_visible
-
-    def _set_always_visible(self, always_visible):
-        self._always_visible = always_visible
-
-    always_visible = property(_get_always_visible, _set_always_visible)
-
-
-
-
-    
+        self.x = x
+        self.y = y
+        self.ch = ch
+        self.name = name
+        self.color = color
+        self.bkg_color = bkg_color
+        self.blocks = blocks
+        self.always_visible = always_visible
 
     def draw(self, visible_tiles, console):
-        if (self._x, self._y) in visible_tiles or self._always_visible:
-            console.draw_char(self._x, self._y, self._ch, fg=self._color, bg=self._bkg_color)
+        if (self.x, self.y) in visible_tiles or self.always_visible:
+            console.draw_char(self.x, self.y, self.ch, fg=self.color, bg=self.bkg_color)
