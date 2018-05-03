cc806eb2585bd482c86a1e6406f4c71213377d04
Coul33t <Coulis1990@gmail.com>
2018-05-03 11:25:43 +0200

    Added FPS limitation

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import pdb
 if __name__ == '__main__':
     eng = Engine()
     eng.initialization()
-
+    tdl.set_fps(60)
+    tdl.flush()
+    
     while not tdl.event.isWindowClosed():
         stop = eng.update()
         
